f7a4548a953a7ae670abdf27ed79ea58b1facd76
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-04-16 15:02:30 +0200

    initial sidebar ui

--- frontend/src/pages/layouts/MainLayout.jsx
+++ frontend/src/pages/layouts/MainLayout.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import Sidebar from '@/components/sidebar/Sidebar';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+
+      {/* Mobile Navigation Overlay */}
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-slate-950/50 z-40 block md:hidden"
+          onClick={() => setSidebarOpen(false)}
+          aria-hidden="true"
+        ></div>
+      )}
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* <Header 
+          toggleSidebar={toggleSidebar} 
+          title={getPageTitle()}
+          user={user}
+        /> */}
+
+        {/* Main Content */}
+        <main className="flex-1 overflow-y-auto p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
