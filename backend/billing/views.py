1e2370c44fe4e62abf4b866e7b9661e58baf2175
hamseldakrory <hamseldakrory14@gmail.com>
2025-04-22 21:45:07 +0200

    feat(billing): add search functionality to PaymentViewSet

--- backend/billing/views.py
+++ backend/billing/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from rest_framework.exceptions import ValidationError
 from .models import Payment
 from .serializers import PaymentSerializer
@@ -7,10 +7,12 @@ class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['method','status','amount','patient__first_name','patient__last_name','appointment__id',]
+
 
     def perform_create(self, serializer):
         appointment = serializer.validated_data.get('appointment')
         if Payment.objects.filter(appointment=appointment, status='Paid').exists():
             raise ValidationError("This appointment has already been paid for.")
-
         serializer.save()
