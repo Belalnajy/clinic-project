5bca751254db8ef41f4c5f2b8dd00baa286c5d23
Ahmed Mohamed Moqawi <ahmedmuqawi@gmail.com>
2025-04-24 18:14:20 +0200

    feat(medical_records): add filtering and latest record retrieval to MedicalRecordViewSet feat(medications): implement filtering for MedicationViewSet

--- backend/medications/views.py
+++ backend/medications/views.py
@@ -1,8 +1,15 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import MedicationFilter
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Medication
 from .serializers import MedicationSerializer
+from rest_framework.permissions import IsAuthenticated
+from core.permissions import IsDoctorOrManager
 
 class MedicationViewSet(viewsets.ModelViewSet):
     queryset = Medication.objects.all()
     serializer_class = MedicationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MedicationFilter
+    permission_classes = [IsAuthenticated, IsDoctorOrManager]
